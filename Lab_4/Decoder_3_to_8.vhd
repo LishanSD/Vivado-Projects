6fb8e9ee0e7a5fa6e2983d5dc48d6ccc286c7479
Lishan Dissanayake <dissanayake.lishan@gmail.com>
2023-09-07 13:30:55 +0530

    Minor change

--- Lab_4/Decoder_3_to_8.vhd
+++ Lab_4/Decoder_3_to_8.vhd
@@ -1,35 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/27/2023 10:37:07 PM
--- Design Name: 
--- Module Name: Decoder_3_to_8 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity Decoder_3_to_8 is
     Port ( I : in STD_LOGIC_VECTOR (2 downto 0);
