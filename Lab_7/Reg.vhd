289bb73aeefdc26a56786df830eab10b3a56c783
Lishan Dissanayake <dissanayake.lishan@gmail.com>
2023-09-08 16:36:00 +0530

    Register

--- Lab_7/Reg.vhd
+++ Lab_7/Reg.vhd
@@ -1,3 +1,14 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 
 entity Reg is
     Port ( D : in STD_LOGIC_VECTOR (3 downto 0);
